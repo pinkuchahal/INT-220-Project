516b95e2a3361db94a532b26d6183ea83475b805
pinkuchahal <62691954+pinkuchahal@users.noreply.github.com>
2021-04-10 13:09:45 +0530

    final commit

---
+++ Template/_banner-area.php
@@ -0,0 +1,18 @@
+<!-- Owl-carousel -->
+<section id="banner-area">
+  <div class="owl-carousel owl-theme">
+    <div class="item">
+      <img src="./assets/Banner2.png" alt="Banner2">
+    </div>
+    <div class="item">
+      <img src="./assets/b7.png" alt="Banner3">
+    </div>
+    <div class="item">
+      <img src="./assets/banner1.png" alt="Banner3">
+    </div>
+    <div class="item">
+      <img src="./assets/b2.png" alt="Banner3">
+    </div>
+  </div>
+</section>
+<!-- !Owl-carousel -->
